685667374f9453e6898644a519c27eb89c61d04c
Lav989 <lavanyadasari989@gmail.com>
2025-08-31 22:33:56 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,8 @@ app.use('/api/users', require('./routes/user'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/appointments', require('./routes/appointmentBook')); // Updated route
 
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
